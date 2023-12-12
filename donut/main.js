e26d6a0328606e69cc26b9aa3a16da1f5f58809a
Timur Duishenaliiev <timaster999@gmail.com>
2023-12-12 13:39:39 +0200

    add files

--- donut/main.js
+++ donut/main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const pre = document.createElement("pre");
+document.body.appendChild(pre);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let x = 1760,
+    z = 0,
+    y = 0;
+setInterval(() => {
+  z += .09,
+  y += .01;
+  const a = [...new Array(x)].map((a, r) => r % 80 === 79 ? "\n" : " "),
+    r = new Array(x).fill(0), t = Math.cos(z),
+    e = Math.sin(z), n = Math.cos(y),
+    o = Math.sin(y);
+    for (let s = 0; s < 6.28; s += .07) {
+      const c = Math.cos(s),
+            h = Math.sin(s);
+      for (let s = 0; s < 6.28; s += .02) {
+        const v = Math.sin(s),
+              M = Math.cos(s),
+              i = c + 2,
+              l = 1 / (v * i * e + h * t + 5),
+              p = v * i * t - h * e,
+              d = 0 | 40 + 30 * l * (M * i * n - p * o),
+              m = 0 | 12 + 15 * l * (M * i * o + p * n),
+              f = 0 | 8 * ((h * e - v * c * t) * n - v * c * e - h * t - M * c * o),
+              y = d + 80 * m;
+              m < 22 && m >= 0 && d >= 0 && d < 79 && l > r[y] && (r[y] = l,
+              a[y] = ".,-~:;=!*#$@"[f > 0 ? f : 0])
+      }
+    } pre.innerHTML = a.join("")
+}, 40);
